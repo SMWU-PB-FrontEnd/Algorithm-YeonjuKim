7e48fb28bccd83e9af9c46c233db1a41fa1987f7
Yeonju Kim <83276825+jxxnixx@users.noreply.github.com>
2024-06-21 02:27:19 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -0,0 +1,10 @@
+def solution(start_num, end_num):
+    
+    array = []
+    
+    for i in range(start_num, end_num + 1) :
+        array.append(i)
+        
+    answer = array
+    
+    return answer
